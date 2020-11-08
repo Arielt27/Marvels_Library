2b632868d84f16e1d81e425a69fbff20bb565294
Ariel <arielt2013@hotmail.com>
2020-11-07 22:11:48 -0600

    Creadas funciones para los botones, y asignadas a cada boton.

--- app/src/main/java/com/example/marvelslibrary/MainActivity.kt
+++ app/src/main/java/com/example/marvelslibrary/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.marvelslibrary
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity()
@@ -11,6 +12,47 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setContentView(R.layout.screen2)
+    }
+
+    fun Heroes(view: View)
+    {
+        setContentView(R.layout.screenheroes)
+
+    }
+
+    fun Villanos(view: View)
+    {
+        setContentView(R.layout.villains_screen)
+
+    }
+
+    fun Anti(view: View)
+    {
+        setContentView(R.layout.antiheroes_screen)
+
+    }
+
+    fun Comics(view: View)
+    {
+        setContentView(R.layout.comics_screen)
+
+    }
+
+    fun Buscar(view: View)
+    {
+
+    }
+
+    fun Regresar(view: View)
+    {
+        setContentView(R.layout.screen2)
+
+
+    }
+
+    fun NuevaBusqueda(view: View)
+    {
 
     }
 }
